cb2a0493ddbfd6e1b3ed0ab43c4da8ee5fcf1328
Jim <jimba.ed@outlook.com>
2022-11-04 12:23:15 -0400

    commit tree 211a8c06c130d4d0f4de78a7238bfe042afa0709

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
-
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else
+      item = Item.find(params[:id])
+    end
+      
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(params.permit(:name, :description, :price))
+    render json: item, status: :created
   end
 
 end
